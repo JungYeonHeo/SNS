ef4ad0b9c3450e3b0d90c36142849d139b9db88d
hjy <heojungyeon.97@gmail.com>
2022-07-22 22:00:59 +0900

    Refactor: 회원가입, 로그인 입력값 검증 수정

--- utils/userValidator.js
+++ utils/userValidator.js
@@ -4,14 +4,23 @@ const response = require("./response");
 const checkJoin = [
   body("userId")
     .trim()
+    .notEmpty()
+    .withMessage(response.EMAIL_EMPTY)
+    .bail()
     .isEmail()
     .withMessage(response.EMAIL_WARNING), 
   body("userPw")
     .trim()
+    .notEmpty()
+    .withMessage(response.PW_EMPTY)
+    .bail()
     .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/g)
     .withMessage(response.PW_WARNING),
   body("confirmPw")
     .trim()
+    .notEmpty()
+    .withMessage(response.CONFIRM_PW_EMPTY)
+    .bail()
     .custom((value, {req}) => {
       if (value !== req.body.userPw) {
         throw new Error(response.COMFIRM_PW_WARNING)
@@ -20,17 +29,30 @@ const checkJoin = [
     }),
   body("userName")
     .trim()
+    .notEmpty()
+    .withMessage(response.USERNAME_EMPTY)
+    .bail()
     .isLength({ min: 2, max: 10 })
-    .withMessage(response.NAME_WARNING),
+    .withMessage(response.USERNAME_LENGTH)
+    .bail()
+    .not().matches(/\<|\>|\"|\'|\%|\;|\&|\+|\-/g)
+    .not().matches(/[&\\+\-%@=\/\\\:;,\.\'\"\^`~\_|\!\/\?\*$<>()\[\]\{\}]/i)
+    .withMessage(response.USERNAME_INCLUDE_SCRIPT),
 ];
 
 const checkLogin = [
   body("userId")
     .trim()
+    .notEmpty()
+    .withMessage(response.EMAIL_EMPTY)
+    .bail()
     .isEmail()
     .withMessage(response.EMAIL_WARNING),
   body("userPw")
     .trim()
+    .notEmpty()
+    .withMessage(response.PW_EMPTY)
+    .bail()
     .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/g)
     .withMessage(response.PW_WARNING), 
 ];
